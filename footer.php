73495d433e7a4edc3f35a51ee2c409d0738238ec
everaldomatias <matiaseveraldo@gmail.com>
2017-08-09 23:25:20 -0300

    Sprint iniciando rodape com form de contato.

--- footer.php
+++ footer.php
@@ -14,7 +14,17 @@
 	</div><!-- #wrapper -->
 
 	<footer id="footer" role="contentinfo">
-		<div class="container">
+		<div class="container-fluid nopadding">
+			<div class="infos col-sm-12">
+				<h2 class="entry-title">Contact Us</h2>
+				<h4 class="entry-title">Lipsum consequat at nunc</h4>
+				<?php
+				  	// Verifica se o plugin Contact Form 7 está ativo
+					if( defined( 'WPCF7_PLUGIN' ) ){
+				  		echo do_shortcode( '[contact-form-7 title="Rodapé Contato"]' );
+				  	}
+				?>
+			</div><!-- infos -->
 			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
 		</div><!-- .container-fluid -->
 	</footer><!-- #footer -->
